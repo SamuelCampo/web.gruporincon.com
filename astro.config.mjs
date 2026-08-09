fbd58a23a7d1fc995da531ef9d81e41880a95a8a
Samuel Campo ( GR ) <samuelcampo18@gmail.com>
2026-08-09 09:15:19 -0400

    Feature add sites-maps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 import partytown from "@astrojs/partytown";
 
+
 export default defineConfig({
   site: process.env.SITE_URL || "https://gruporincon.com.co",
   integrations: [
+    sitemap(),
     partytown({
       config: {
         forward: ["dataLayer.push", "gtag"],
